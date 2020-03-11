1b71fb121e47fb3dc1b87b93cce1503fb25ce9b1
Christoph Buck <christoph.buck@smartoptics.de>
2020-03-11 21:04:52 +0100

    Update corona script

--- corona.R
+++ corona.R
@@ -6,9 +6,13 @@ library(janitor)
 # hopkins dataset
 ###https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
 
-filename <- "data/time_series_19-covid-Confirmed_05_03_20.csv"
+filename <- "data/time_series_19-covid-Confirmed_10_03_20.csv"
 start_date <- as.POSIXct("2020-01-22")
-number_of_days <- 44
+number_of_days <- 49
+
+# Generate timestamp
+days <- format(seq(start_date, by="day", length.out=number_of_days), "%m/%d/%y")
+days<-gsub("0(\\d)", "\\1", days)
 
 # Read in data
 dfhop <- read.table(filename, check.names=FALSE,  header=TRUE,sep=",")
@@ -25,9 +29,6 @@ china_total <- data.frame("Province/State"="","Country/Region"="China Total",Lat
 dfhop <- rbind(dfhop, china_total)
 
 
-# Generate timestamp
-days <- format(seq(start_date, by="day", length.out=number_of_days), "%m/%d/%y")
-days<-gsub("0(\\d)", "\\1", days)
 
 # Convert dates into varibale
 dfhop <- gather(dfhop,all_of(days),key="date",value="number")
@@ -40,27 +41,62 @@ colnames(dfhop) <- c("Province/State", "Country","Lat","Long","Date","Number")
 
 
 dfhop <- dfhop[dfhop$Country == "Germany"
-#            |  dfhop$Country == "Italy"
+             |  dfhop$Country == "Italy"
              |  dfhop$Country == "Spain"
              |  dfhop$Country == "France"
              |  dfhop$Country == "Austria"
-#            |  dfhop$Country == "South Korea"
+             |  dfhop$Country == "South Korea"
              |  dfhop$Country == "UK"
              |  dfhop$Country == "US Total"
-#            |  dfhop$Country == "China Total"
-#            |  dfhop$Country == "Iran"
+             |  dfhop$Country == "China Total"
+             |  dfhop$Country == "Iran"
               ,]
 
 
 dfhop_lastx <- dfhop[dfhop$Date > as.POSIXct(Sys.Date(),format="%m/%d/%y")
                      - as.difftime(15, unit = "days"),]
 
+growth_rate = dfhop %>%
+  # first sort by year
+  arrange(Date) %>% filter(Country == "Germany") %>%
+  mutate(Diff_Date = Date - lag(Date),  
+         Diff_Growth = Number - lag(Number),
+         Rate_percent = (Diff_Growth / 1)/Number * 100) 
+
+growth_rate <- rbind(growth_rate,dfhop %>%
+  arrange(Date) %>% filter(Country == "Italy") %>%
+  mutate(Diff_Date = Date - lag(Date), 
+         Diff_Growth = Number - lag(Number),
+         Rate_percent = (Diff_Growth / 1)/Number * 100))
+
+growth_rate <- rbind(growth_rate,dfhop %>%
+  arrange(Date) %>% filter(Country == "France") %>%
+  mutate(Diff_Date = Date - lag(Date), 
+         Diff_Growth = Number - lag(Number),
+         Rate_percent = (Diff_Growth / 1)/Number * 100))
+
+growth_rate <- rbind(growth_rate,dfhop %>%
+  arrange(Date) %>% filter(Country == "US Total") %>%
+  mutate(Diff_Date = Date - lag(Date), 
+         Diff_Growth = Number - lag(Number),
+         Rate_percent = (Diff_Growth / 1)/Number * 100))
+
+growth_rate <- rbind(growth_rate,dfhop %>%
+  arrange(Date) %>% filter(Country == "Spain") %>%
+  mutate(Diff_Date = Date - lag(Date), 
+         Diff_Growth = Number - lag(Number),
+         Rate_percent = (Diff_Growth / 1)/Number * 100))
+
 
 plot <- ggplot(dfhop, aes(x = Date, y = Number)) +
     geom_line(aes(color=Country)) +
     geom_point()
 
-plot <- ggplot(dfhop_lastx, aes(x = Date, y = Number)) +
-    geom_line(aews(color=Country)) +
+plot_lastx <- ggplot(dfhop_lastx, aes(x = Date, y = Number)) +
+    geom_line(aes(color=Country)) +
     geom_point()
 
+
+plot_growth <- ggplot(growth_rate, aes(x = Date, y = Diff_Growth)) +
+    geom_line(aes(color=Country)) +
+    geom_point()
