d1739f2d7706c649e9199c612ea1b1338e99328b
IcePic <icepic1984@gmx.de>
2020-03-15 17:28:22 +0100

    Update script

--- corona.R
+++ corona.R
@@ -50,7 +50,7 @@ confirmed <- "data/time_series_19-covid-Confirmed.csv"
 death <- "data/time_series_19-covid-Deaths.csv"
 recovered <- "data/time_series_19-covid-Recovered.csv"
 
-days <- generate_days(as.POSIXct("2020-01-22"),51)
+days <- generate_days(as.POSIXct("2020-01-22"),53)
 
 # Read in data
 dfhop_confirmed <- read.table(confirmed, check.names=FALSE,  header=TRUE,sep=",", quote = "\"")
@@ -58,17 +58,17 @@ dfhop_death <- read.table(death, check.names=FALSE,  header=TRUE,sep=",", quote=
 dfhop_recovered <- read.table(recovered, check.names=FALSE,  header=TRUE,sep=",",quote = "\"")
 
 dfhop_confirmed <- calculate_total(dfhop_confirmed,days, "US", "US Total")
-dfhop_confirmed <- calculate_total(dfhop_confirmed,days, "Mainland China", "China Total")
+dfhop_confirmed <- calculate_total(dfhop_confirmed,days, "China", "China Total")
 dfhop_confirmed <- calculate_total(dfhop_confirmed,days, "Denmark", "Denmark Total")
 dfhop_confirmed <- calculate_total(dfhop_confirmed,days, "France", "France Total")
 
 dfhop_death <- calculate_total(dfhop_death,days, "US", "US Total")
-dfhop_death <- calculate_total(dfhop_death,days, "Mainland China", "China Total")
+dfhop_death <- calculate_total(dfhop_death,days, "China", "China Total")
 dfhop_death <- calculate_total(dfhop_death,days, "Denmark", "Denmark Total")
 dfhop_death <- calculate_total(dfhop_death,days, "France", "France Total")
 
 dfhop_recovered <- calculate_total(dfhop_recovered,days, "US", "US Total")
-dfhop_recovered <- calculate_total(dfhop_recovered,days, "Mainland China", "China Total")
+dfhop_recovered <- calculate_total(dfhop_recovered,days, "China", "China Total")
 dfhop_recovered <- calculate_total(dfhop_recovered,days, "Denmark", "Denmark Total")
 dfhop_recovered <- calculate_total(dfhop_recovered,days, "France", "France Total")
 
@@ -93,22 +93,22 @@ dfhop <- rbind(
     calculate_growth(dfhop,"Spain"),
     calculate_growth(dfhop,"UK"),
     calculate_growth(dfhop,"US Total"),
-     # calculate_growth(dfhop,"Republic of Korea"),
+    calculate_growth(dfhop,"Korea, South"),
     calculate_growth(dfhop,"Sweden"),
     calculate_growth(dfhop,"Denmark Total"),
     calculate_growth(dfhop,"Norway"),
     calculate_growth(dfhop,"Netherlands"),
     calculate_growth(dfhop,"France Total"),
-    calculate_growth(dfhop,"Iran (Islamic Republic of)")
-      #calculate_growth(dfhop,"China Total")
+    calculate_growth(dfhop,"Iran (Islamic Republic of)"),
+    calculate_growth(dfhop,"China Total")
       )
 
 #moving average
-dfhop_mean <- dfhop %>% filter(Country == "Italy") %>%
+dfhop_mean <- dfhop %>% filter(Country == "Korea, South") %>%
     mutate(Mean_Growth=rollapply(
                Growth_Confirmed,4,mean,align='right',fill=NA),
            Mean_Rate=rollapply(
-               Rate_Confirmed,4,mean,align='right',fill=NA),)
+               Rate_Confirmed,4,mean,align='right',fill=NA))
 
 plot_mean <- ggplot(dfhop_mean, aes(x = Date, y = Mean_Rate)) +
     geom_line(aes(color=Country)) +
